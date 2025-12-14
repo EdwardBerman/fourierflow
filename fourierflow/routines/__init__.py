71aecbb6b4daeaa6f478fa379b7480c2a5b3dfea
EdwardBerman <10emb29@gmail.com>
2025-12-14 16:06:26 -0500

    new base

--- fourierflow/routines/__init__.py
+++ fourierflow/routines/__init__.py
@@ -1,10 +1,6 @@
 from .grid_2d_markov import Grid2DMarkovExperiment
 from .grid_2d_rollout import Grid2DRolloutExperiment
-try:
-    from .learned_interpolator import LearnedInterpolator
-except ModuleNotFoundError:
-    # Optional JAX stack (haiku, optax, jax)
-    LearnedInterpolator = None
+from .learned_interpolator import LearnedInterpolator
 from .meshgraphnet_jax import MeshGraphNet
 from .point_cloud import PointCloudExperiment
 from .structured_mesh import StructuredMeshExperiment
